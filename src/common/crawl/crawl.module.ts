d36945a823721089049a10ce63875e2d2b808eb4
Jgone2 <jgoneit@gmail.com>
2024-09-19 18:36:12 +0900

    :fire:: remove: no xy place

--- src/common/crawl/crawl.module.ts
+++ src/common/crawl/crawl.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CrawlService } from './crawl.service';
 import { CrawlController } from './crawl.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [CrawlController],
   providers: [CrawlService],
 })
