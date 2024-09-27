13f736c18910c8c9cfae02d7e19964fb199b36c3
Jgone2 <jgoneit@gmail.com>
2024-09-27 17:10:38 +0900

    :sparkles:: feat: can use favorite func

--- src/common/favorites/favorites.controller.ts
+++ src/common/favorites/favorites.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
+import { User } from '../decorator/user/user.decorator';
+import { AccessTokenGuard } from '../../auth/guard/bearer-token.guard';
 
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Post('toggle/:placeId')
+  @UseGuards(AccessTokenGuard)
+  async toggleFavorite(
+    @User('email') userEmail: string,
+    @Param('restaurantId', ParseIntPipe) placeId: number,
+  ) {
+    return this.favoritesService.toggleFavorite(userEmail, placeId);
+  }
+
+  @Get(':placeId')
+  @UseGuards(AccessTokenGuard)
+  async isFavorite(
+    @User('email') userEmail: string,
+    @Param('restaurantId', ParseIntPipe) placeId: number,
+  ) {
+    return this.favoritesService.isFavorite(userEmail, placeId);
+  }
 }
--- src/common/favorites/favorites.service.ts
+++ src/common/favorites/favorites.service.ts
@@ -1,4 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FavoritesEntity } from '../entities/favorites.entity';
+import { Repository } from 'typeorm';
+import { UsersService } from '../../users/users.service';
+import { PlaceService } from '../../place/place.service';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  constructor(
+    @InjectRepository(FavoritesEntity)
+    private readonly favoritesRepository: Repository<FavoritesEntity>,
+    private readonly userService: UsersService,
+    private readonly placeService: PlaceService,
+  ) {}
+
+  async addFavorite(userEmail: string, placeId: number) {
+    const findUser = await this.userService.findUserByEmail(userEmail);
+    const findPlace = await this.placeService.findRestaurantById(placeId);
+    const favorite = this.favoritesRepository.create({
+      userId: findUser.id,
+      restaurantId: findPlace.id,
+    });
+    return await this.favoritesRepository.save(favorite);
+  }
+
+  async removeFavorite(userEmail: string, placeId: number) {
+    const findUser = await this.userService.findUserByEmail(userEmail);
+    const findPlace = await this.placeService.findRestaurantById(placeId);
+    const favorite = await this.favoritesRepository.findOne({
+      where: { userId: findUser.id, restaurantId: findPlace.id },
+    });
+    return await this.favoritesRepository.remove(favorite);
+  }
+
+  async isFavorite(userEmail: string, placeId: number) {
+    const findUser = await this.userService.findUserByEmail(userEmail);
+    const findPlace = await this.placeService.findRestaurantById(placeId);
+    const favorite = await this.favoritesRepository.findOne({
+      where: { userId: findUser.id, restaurantId: findPlace.id },
+    });
+    return favorite ? 1 : 0; // 'favorite' 객체가 존재하면 1, 존재하지 않으면 0
+  }
+
+  async toggleFavorite(userEmail: string, placeId: number): Promise<number> {
+    const findUser = await this.userService.findUserByEmail(userEmail);
+    const findPlace = await this.placeService.findRestaurantById(placeId);
+
+    const existingFavorite = await this.favoritesRepository.findOne({
+      where: { userId: findUser.id, restaurantId: findPlace.id },
+    });
+
+    if (existingFavorite) {
+      // 이미 좋아요가 존재하면 삭제
+      await this.favoritesRepository.remove(existingFavorite);
+      return 0; // 좋아요 취소 후 0 반환
+    } else {
+      // 좋아요가 없다면 새로 추가
+      const newFavorite = this.favoritesRepository.create({
+        userId: findUser.id,
+        restaurantId: findPlace.id,
+      });
+      await this.favoritesRepository.save(newFavorite);
+      return 1; // 좋아요 추가 후 1 반환
+    }
+  }
+}
