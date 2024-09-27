0a36aeb4b5f7d3ee0ec65627f7ed94fec6310152
Jgone2 <jgoneit@gmail.com>
2024-09-27 17:03:58 +0900

    :bug:: fix: add authmoudles at favorites module and likes module

--- src/common/favorites/favorites.module.ts
+++ src/common/favorites/favorites.module.ts
@@ -3,14 +3,16 @@ import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesEntity } from '../entities/favorites.entity';
-import { ReviewsModule } from '../../reviews/reviews.module';
 import { UsersModule } from '../../users/users.module';
+import { PlaceModule } from '../../place/place.module';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([FavoritesEntity]),
-    ReviewsModule,
+    PlaceModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
--- src/common/likes/likes.module.ts
+++ src/common/likes/likes.module.ts
@@ -5,9 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikeEntity } from '../entities/like.entity';
 import { ReviewsModule } from '../../reviews/reviews.module';
 import { UsersModule } from '../../users/users.module';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LikeEntity]), ReviewsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([LikeEntity]),
+    ReviewsModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [LikesController],
   providers: [LikesService],
   exports: [LikesService],
