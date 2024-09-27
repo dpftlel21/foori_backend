c8a43687c3b53df8e67424b8ab00188f2b033898
Jgone2 <jgoneit@gmail.com>
2024-09-27 17:00:17 +0900

    :fire:: remove: authModule

--- src/common/likes/likes.module.ts
+++ src/common/likes/likes.module.ts
@@ -4,17 +4,12 @@ import { LikesController } from './likes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikeEntity } from '../entities/like.entity';
 import { ReviewsModule } from '../../reviews/reviews.module';
-import { AuthModule } from '../../auth/auth.module';
 import { UsersModule } from '../../users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([LikeEntity]),
-    ReviewsModule,
-    AuthModule,
-    UsersModule,
-  ],
+  imports: [TypeOrmModule.forFeature([LikeEntity]), ReviewsModule, UsersModule],
   controllers: [LikesController],
   providers: [LikesService],
+  exports: [LikesService],
 })
 export class LikesModule {}
