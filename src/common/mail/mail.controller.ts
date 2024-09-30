e73e6879d9037c8bbf10acfec3c175cad9efcf64
Jgone2 <jgoneit@gmail.com>
2024-09-30 12:17:15 +0900

    :sparkles:: feat: create post request

--- src/common/mail/mail.controller.ts
+++ src/common/mail/mail.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MailService } from './mail.service';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
+
+  // 1. 메일 전송 API
+  @Post('send-verification')
+  async sendVerificationEmail(@Body('email') email: string) {
+    return this.mailService.sendVerificationEmail(email);
+  }
 }
