276b031b2698bb849c697489888bbf033e710d41
Jgone2 <jgoneit@gmail.com>
2024-10-25 09:41:08 +0900

    :recycle:: refactor: add global prefix to path api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
