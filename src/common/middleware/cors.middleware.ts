f8208b379df92d47d492440d9f63f6cc16e59f35
Jgone2 <jgoneit@gmail.com>
2024-10-28 12:49:38 +0900

    :recycle:: refactor add preflight

--- src/common/middleware/cors.middleware.ts
+++ src/common/middleware/cors.middleware.ts
@@ -3,17 +3,30 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    res.header('Access-Control-Allow-Origin', [
+    const allowedOrigins = [
       'https://foori.co.kr',
       'https://www.foori.co.kr',
       'https://localhost:5361',
       'https://52.79.150.195:3001',
-    ]);
+    ];
+
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+
     res.header(
       'Access-Control-Allow-Methods',
       'GET, POST, PATCH, PUT, DELETE, OPTIONS',
     );
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    // OPTIONS 메서드에 대한 프리플라이트 요청 처리
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+
     next();
   }
 }
