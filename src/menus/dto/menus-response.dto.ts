c8ad5a06922e86c590726884b84b60d6a76e1706
Jgone2 <jgoneit@gmail.com>
2025-01-02 16:46:15 +0900

    :recycle:: refactor:: add id menu response dto

--- src/menus/dto/menus-response.dto.ts
+++ src/menus/dto/menus-response.dto.ts
@@ -2,6 +2,10 @@ import { IsNumber, IsString } from 'class-validator';
 import { Expose } from 'class-transformer';
 
 export class MenusResponseDto {
+  @IsNumber()
+  @Expose()
+  id: number;
+
   @IsString()
   @Expose()
   name: string;
