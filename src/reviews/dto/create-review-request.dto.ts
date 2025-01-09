2d7d9cd4fbad043e7ba5273c40467c5fd4dc20db
Jgone2 <jgoneit@gmail.com>
2025-01-09 23:06:03 +0900

    :sparkles:: feat: add transformation for form data string to number

--- src/reviews/dto/create-review-request.dto.ts
+++ src/reviews/dto/create-review-request.dto.ts
@@ -1,11 +1,19 @@
-import { IsNumber, IsString, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
 export class CreateReviewRequestDto {
   @IsString()
+  @IsNotEmpty()
   content: string;
+
   @IsNumber()
+  @IsNotEmpty()
   @Min(0)
   @Max(5)
+  @Type(() => Number) // ParseIntPipe 대신 @Type 사용
   rating: number;
+
   @IsNumber()
+  @IsNotEmpty()
+  @Type(() => Number) // ParseIntPipe 대신 @Type 사용
   bookingId: number;
 }
